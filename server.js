fb9625e0d6c15f158377119f01f20b7877be1fdb
Kami <51175640+hfurusho@users.noreply.github.com>
2019-12-03 23:40:32 -0800

    New notes get saved to db.json file.

--- server.js
+++ server.js
@@ -7,26 +7,45 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const writeFileAsync = util.promisify(fs.writeFile);
 const readFileAsync = util.promisify(fs.readFile);
+const dbPath = path.join(__dirname, "db", "db.json");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
-app.get("*", function(req, res) {
+app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/api/notes", function(req, res) {
-  // console.log(path.join(__dirname, "db", "db.json"));
-  // console.log(db);
-  const db = fs.readFileAsync(path.join(__dirname, "db", "db.json"));
-  console.log(db);
-  console.log("Hello?");
-  return res.json(db);
+app.get("/api/notes", async function(req, res) {
+  try {
+    const notes = getNotes();
+    console.log(notes);
+    return res.send(notes);
+  } catch (err) {
+    console.log(err);
+  }
 });
+
+app.post("/api/notes", function(req, res) {
+  let notes = getNotes();
+  notes.push(req.body);
+  console.log(notes);
+  writeFileAsync(dbPath, JSON.stringify(notes));
+  res.send(notes);
+});
+
+// app.get("/api/notes/:id")
+
+function getNotes() {
+  let notes = fs.readFileSync(dbPath, "utf8");
+  notes = JSON.parse(notes);
+  return notes;
+}
 
 app.listen(PORT, function() {
   console.log("App listening on PORT http://localhost:" + PORT);
